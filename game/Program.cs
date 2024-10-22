d0f9a326be989aab9ee3f2b29c37e6ec009194fd
Marcin Puzoń <marcinpzon@gmail.com>
2024-10-22 23:46:23 +0200

    Create Program.cs

---
+++ game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            System.Console.WriteLine("Hello world");
+        }
+    }
+}
