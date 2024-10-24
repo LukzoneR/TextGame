3a4c930ef9059067028d44cdbea9aeab6f9c9dcd
Marcin Puzoń <marcinpzon@gmail.com>
2024-10-24 22:13:06 +0200

    commit 1

--- TextGame/src/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ TextGame/src/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e935c4601e6a8b921ce080bf3f2907c94113e90b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
